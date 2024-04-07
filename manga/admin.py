7143ba3f9335ed31757eddab321327f132193c46
ted111712 <ted111712@gmail.com>
2024-04-08 03:45:58 +0900

    Feat: 어드민 페이지에서 검색 기능 추가
     - 어드민 페이지에서 Manga 모델 한정 제목으로 검색 기능 추가

--- manga/admin.py
+++ manga/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Manga
+from .models import Manga, Publisher
+
 
 # Register your models here.
-admin.site.register(Manga)
+class MangaAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+
+
+admin.site.register(Manga, MangaAdmin)
+admin.site.register(Publisher)
