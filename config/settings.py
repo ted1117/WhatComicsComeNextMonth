be1f8e766ec071cd3e7e8ffcf30a4f87ce82cd6c
ted111712 <ted111712@gmail.com>
2024-04-11 22:48:47 +0900

    Chore: .gitignore 업데이트
     - static 폴더 추가

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = []
+
+STATIC_ROOT = BASE_DIR / "static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Silk
+SILKY_PYTHON_PROFILER = True
