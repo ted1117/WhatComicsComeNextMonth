3ae94e13d92402967031efe12d87b4819b43b20e
ted111712 <ted111712@gmail.com>
2024-11-14 19:01:08 +0900

    Feat: 데이터베이스를 sqlite3에서 PostgreSQL로 병합

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "django_filters",
     "corsheaders",
     "django_celery_beat",
+    "django_extensions",
     "django_rename_app",
 ]
 
@@ -136,8 +137,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
