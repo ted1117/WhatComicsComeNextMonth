8faa9480716b67d850e0a3a1a6f853db144a4ce9
ted111712 <ted111712@gmail.com>
2025-03-05 15:19:51 +0900

    Feat: 로그인 상태 확인 뷰 추가

--- user/urls.py
+++ user/urls.py
@@ -10,5 +10,6 @@ router.register(r"", views.UserViewset, basename="user")
 urlpatterns = [
     path("login/", views.UserLoginView.as_view()),
     path("logout/", views.UserLogoutView.as_view()),
+    path("check-login-status/", views.check_login_status, name="check_login_status"),
     path("", include(router.urls)),
 ]
