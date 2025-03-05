23e121dbaf2549be2bfb4dd64882124217ea8503
ted111712 <ted111712@gmail.com>
2025-03-05 15:16:21 +0900

    Fix: 비로그인 상태에서 뷰에 접근할 수 없는 오류 수정

--- user/authentication.py
+++ user/authentication.py
@@ -1,7 +1,6 @@
 from typing import Any
 
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
-from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.request import Request
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
@@ -17,7 +16,7 @@ class CustomJWTAuthentication(JWTAuthentication):
         try:
             validated_token: Token = self.get_validated_token(raw_token)
         except (InvalidToken, TokenError) as e:
-            raise AuthenticationFailed("유효하지 않은 토큰입니다.") from e
+            return None
 
         return self.get_user(validated_token), validated_token
 
